2b18134910addaedf903174e9f4407146e2169bb
Shanmugam275 <144407792+Shanmugam275@users.noreply.github.com>
2023-09-21 10:13:07 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,11 +1,21 @@
-def isLeapYear(year):
-  if (year % 4 == 0 and year % 100 !=0) or year % 400 == 0:
-    return True 
-  else:
-    return False
+class Player:
+    def play(self):
+        print("The player is playing cricket.")
 
-year = 2000
-if isLeapYear(year):
-  print('{} is a leap year.'.format(year))
-else:
-  print('{} is not a leap year.'.format(year))
+# Define the derived class Batsman
+class Batsman(Player):
+    def play(self):
+        print("The batsman is batting.")
+
+# Define the derived class Bowler
+class Bowler(Player):
+    def play(self):
+        print("The bowler is bowling.")
+
+# Create objects of Batsman and Bowler classes
+batsman = Batsman()
+bowler = Bowler()
+
+# Call the play() method for each object
+batsman.play()
+bowler.play()
